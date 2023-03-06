c80790d7f3aeff7ffc6daec2f9c8c06c618cee36
IliyanSinapov <iliyan.sinapov@gmail.com>
2023-03-06 16:16:12 +0200

    initial commit

--- src/main/resources/static/js/scripts/navigation-script.js
+++ src/main/resources/static/js/scripts/navigation-script.js
@@ -1,3 +1,4 @@
+//region navigation region
 //Desktop device
 const navbar = document.getElementById("desktop-user");
 //Mobile device
@@ -5,23 +6,44 @@ const sidebar = document.getElementById("mobile-user");
 //Navigation span for css
 const span = document.getElementById("navigation-css");
 const link = document.createElement("link");
+//endregion
 
+//region dropdown region
 //dropdown
 const accDropdown  = document.querySelector(".dropdown");
 const dropdownHeader  = document.querySelector(".dropdown-header");
+const dropdown = dropdownHeader.parentElement;
+const dropdownIcon = dropdown.children[1];
+//endregion
 
+//region activeDivVars region
+const activeDiv = document.querySelector(".active");
+const dropdownHeaderValue = dropdownHeader.textContent;
+
+dropdownHeader.addEventListener("mouseover", () => {
+    activeDiv.style.width = (dropdownHeaderValue.length * 1.4) + "rem";
+})
+//endregion
+
+//region dropdownClickFunctions region
 accDropdown.addEventListener("click", (event) => {
     event.preventDefault()
-    const dropdownMenu = event.target.querySelector(".dropdown-menu");
+    const dropdownMenu = dropdown.children[2];
     dropdownMenu.classList.toggle("show");
 })
-
 dropdownHeader.addEventListener("click", (event) => {
     event.preventDefault()
-    const dropdown = dropdownHeader.parentElement;
-    const dropdownMenu = dropdown.children[1];
+    const dropdownMenu = dropdown.children[2];
     dropdownMenu.classList.toggle("show");
+    console.log("clicked")
 })
+dropdownIcon.addEventListener("click", (event) => {
+    event.preventDefault()
+    const dropdownMenu = dropdown.children[2];
+    dropdownMenu.classList.toggle("show");
+    console.log("clicked")
+})
+//endregion
 
 function checkUserDevice() {
     if (screen.width < 811) {
@@ -31,7 +53,7 @@ function checkUserDevice() {
 
         link.setAttribute("rel", "stylesheet");
         link.setAttribute("type", "text/css");
-        link.setAttribute("href", "css/styles/sidebar-style.css")
+        link.setAttribute("href", "/css/styles/sidebar-style.css")
         span.innerHTML = "";
 
         const bodyEl = document.body
@@ -43,7 +65,7 @@ function checkUserDevice() {
     } else {
         link.setAttribute("rel", "stylesheet");
         link.setAttribute("type", "text/css");
-        link.setAttribute("href", "css/styles/navbar-style.css")
+        link.setAttribute("href", "/css/styles/navbar-style.css")
         span.innerHTML = "";
 
         navbar.style.display = "block";
@@ -51,7 +73,6 @@ function checkUserDevice() {
     }
     span.appendChild(link);
 }
-
 setInterval(() => {
     checkUserDevice();
 }, 250);
